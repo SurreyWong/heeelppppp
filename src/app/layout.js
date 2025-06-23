1a26aac916d03a233422e83ff186c388cf0dbdcc
Surrey Wong <104850316+SurreyWong@users.noreply.github.com>
2025-06-24 01:37:48 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 //import { ClerkProvider } from "@clerk/nextjs/dist/types/components.server";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+
 
 
 const geistSans = Geist({
@@ -22,16 +24,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-
-
-    
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          {children}
+          <Toaster richColors position="top-center" />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
